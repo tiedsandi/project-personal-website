30123bb74bbf54902931e16c48266b293501b443
tiedsandi <fachransandi@gmail.com>
2024-11-04 20:44:08 +0700

    update project moviedb

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -4,12 +4,14 @@ import React from 'react';
 import {projects} from '@/data/projectList.json';
 
 const page = () => {
+  const sortProject = projects.sort((a, b) => b.id - a.id);
+
   return (
     <div className='flex flex-col p-3'>
-      <h3 className='font-bold text-2xl underline mb-6 text-center'>Proyek Saya</h3>
+      <h3 className='mb-6 text-2xl font-bold text-center underline'>Proyek Saya</h3>
 
-      <div className='flex flex-wrap gap-6 justify-center'>
-        {projects.map((data, index) => (
+      <div className='flex flex-wrap justify-center gap-6'>
+        {sortProject.map((data, index) => (
           <Card key={index} data={data} />
         ))}
       </div>
